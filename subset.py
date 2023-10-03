cecc8929722bdd2e1f35dbe262ccbbd243608c3f
pragnya163 <146310607+pragnya163@users.noreply.github.com>
2023-10-03 09:42:30 +0530

    Add files via upload

---
+++ subset.py
@@ -0,0 +1,16 @@
+def check(arr,target,n):
+    if target==0:
+        return True
+    if n==0:
+        return False
+    if arr[n-1]>target:
+        return check(arr,target,n-1)
+    return check(arr,target-arr[n-1],n-1) or check(arr,target,n-1)
+arr=[3, 12, 5, 4, 24, 2]
+target=9
+ans=check(arr,target,len(arr))
+print(ans)
+'''if ans==True:
+    print('Found')
+else:
+    print('not found')'''
